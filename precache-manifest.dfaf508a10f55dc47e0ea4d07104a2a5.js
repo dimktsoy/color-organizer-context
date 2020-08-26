f97983dcd6c072556092d6240066a98757c78f22
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-26 21:32:40 +0600

    Updates

---
+++ precache-manifest.dfaf508a10f55dc47e0ea4d07104a2a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba07200df724a6e6a8a51a9c0f27d0f",
+    "url": "/color-organizer-context/index.html"
+  },
+  {
+    "revision": "1279dafcf26e1df22035",
+    "url": "/color-organizer-context/static/css/main.e30ced16.chunk.css"
+  },
+  {
+    "revision": "400fcef03066b63d33e9",
+    "url": "/color-organizer-context/static/js/2.74397e55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-organizer-context/static/js/2.74397e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1279dafcf26e1df22035",
+    "url": "/color-organizer-context/static/js/main.b8ac8fcc.chunk.js"
+  },
+  {
+    "revision": "df8e60f846021ed50ed6",
+    "url": "/color-organizer-context/static/js/runtime-main.57c12275.js"
+  },
+  {
+    "revision": "91bebd82126223907cf0fc243f9668ad",
+    "url": "/color-organizer-context/static/media/color-icon-remove.91bebd82.svg"
+  }
+]);
